08973e46ce32f8b662a0f39b7293e8d359ad9679
aoton0029 <aotonnnnn@gmail.com>
2025-06-23 23:47:38 +0900

    upd

---
+++ CoreLib/DataProcessors/IDataProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.DataProcessors
+{
+    public interface IDataProcessor
+    {
+        Task ScheduleDataProcessing(DataWithKey dataWithKey);
+    }
+}
